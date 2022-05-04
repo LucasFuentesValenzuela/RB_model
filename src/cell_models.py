a7ee0fa1d370b17e167e7bcac3f3443e15ee37b2
Lucas Fuentes <lucasfuentes100@hotmail.com>
2022-05-04 11:45:12 -0700

    playing with model

--- src/cell_models.py
+++ src/cell_models.py
@@ -14,8 +14,9 @@ class cell_v1(object):
 
     def __init__(
             self, alpha=2, beta0=3, delta=1, 
-            gamma=.9, epsilon=.01, RB_thresh=1.1, dt=1e-3, 
-            division="concentration"
+            gamma=.9, epsilon=.01, dt=1e-3, 
+            division="timer", transition="size", 
+            time_SG2 = 1e-1, transition_th=1.
         ):
         """
         Initialize a cell
@@ -24,12 +25,16 @@ class cell_v1(object):
         self.RB = 2  # amount
         self.phase = "G1"
         self.division = division
+        self.transition = transition
 
         if division=="concentration":
-            self.RB_division = self.RB/self.M  # concentration
+            self.division_th = self.RB/self.M  # concentration
         elif division=="amount":
-            self.RB_division = self.RB
-        self.RB_transition = RB_thresh  # in concentration
+            self.division_th = self.RB
+        elif division=="timer":
+            self.division_th = time_SG2
+
+        self.transition_th = transition_th  # in concentration or size
 
         # parameters
         self.params = {
@@ -58,6 +63,7 @@ class cell_v1(object):
 
     def transit(self):
         self.phase = "G2"
+        self.time_SG2 = 0
         return
 
     def grow(self):
@@ -78,12 +84,18 @@ class cell_v1(object):
             M_tmp = self.M + \
                 self.params["dt"] * self.params["gamma"] * \
                 (self.M)**self.params["delta"]
-            if (self.division=="concentration") and (self.RB_c() > self.RB_division):
-                self.divide()
-            elif (self.division=="amount") and (self.RB > self.RB_division):
-                self.divide()
-            else:
-                self.M = M_tmp
+
+            if self.phase=="G2":
+                self.time_SG2 += self.dt
+
+                if (self.division=="concentration") and (self.RB_c() > self.division_th):
+                    self.divide()
+                elif (self.division=="amount") and (self.RB > self.division_th):
+                    self.divide()
+                elif (self.division=="timer") and (self.time_SG2 > self.division_th):
+                    self.divide()
+                else:
+                    self.M = M_tmp
             return
 
         def step_concentrations(self):
@@ -100,7 +112,9 @@ class cell_v1(object):
             self.RB = self.RB + self.params['dt'] * \
                 (self.params['alpha']*self.M - beta*self.RB)
 
-            if self.RB_c() < self.RB_transition:
+            if (self.transition=="RBc") and (self.RB_c() < self.transition_th):
+                self.transit()
+            elif (self.transition=="size") and (self.M < self.transition_th):
                 self.transit()
 
             return
@@ -141,14 +155,24 @@ class cell_v1(object):
 
         alpha, beta0, epsilon = self.params['alpha'], self.params['beta0'], self.params['epsilon']
 
-        if self.division=="concentration":
+        if self.division=="concentration" and self.transition=="RBc":
             if any([
-                not (alpha/beta0/epsilon > self.RB_division),
-                not (self.RB_division > self.RB_transition),
-                not (self.RB_transition > alpha/beta0)
+                not (alpha/beta0/epsilon > self.division_th),
+                not (self.division_th > self.transition_th),
+                not (self.transition_th > alpha/beta0)
             ]):
                 print("Parameters invalid")
                 return
 
-        elif self.division=="amount":
-            return
+        elif self.division=="amount" and self.transition=="RBc":
+            if any([
+                not (alpha/beta0/epsilon > self.RB_transition), 
+                not(self.RB_transition > alpha/beta0)
+            ]):
+                print("Parameters invalid")
+                print(f"alpha/beta0/epsilon = {alpha/beta0/epsilon}")
+                print(f"self.RB_transition = {self.RB_transition}")
+                print(f"alpha/beta0 = {alpha/beta0}")
+                return
+        else:
+            print("Params check not implemented")
